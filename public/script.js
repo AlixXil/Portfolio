c1aafc7197a76166f7af86f641ee0b564ed6532f
Xil <kuzmin1989@outlook.com>
2020-12-17 23:19:36 +0300

    add public

--- public/script.js
+++ public/script.js
@@ -1,19 +1,38 @@
-let btn = document.querySelector('#getWorks');
-let result = document.querySelector('.result');
+document.addEventListener('DOMContentLoaded', () => {
 
-btn.addEventListener('click', (e) => {
-	e.preventDefault();
+	let worksOutput = document.querySelector('.works-list');
+
 	getData();
+
+	// получаем данные о работах
+	async function getData() {
+		let res = await fetch('/api/works')
+
+
+		// обхект с данными
+		if(res.status == 200) {
+			let data = JSON.parse(await res.json())
+			let worksHTML = '';
+
+			for (i in data)
+				worksHTML += addElement(data[i])
+
+			worksOutput.innerHTML = worksHTML;
+		}
+	}	
+
+
+	function addElement(data) {
+		return `
+		<div class="item">
+			<img src="${data.img}", alt="${data.title}">
+			<div class="item__info">
+				<h4 class="item__title">${data.title}</h4>
+				
+				<span class="item__text">${data.subTitle}</span>
+				<a href="${data.link}"," target="_blank" class="item__link">Посмотреть</a>
+			</div>
+		</div>`;
+	}
 })
 
-// получаем данные о работах
-async function getData() {
-	let res = await fetch('/api/works')
-
-	result.innerHTML = '';
-
-	// обхект с данными
-	let data = JSON.parse(await res.json())
-	for (i in data)
-		result.innerHTML += data[i].title + '<br>'
-}
--- routs/api.js
+++ routs/api.js
@@ -9,16 +9,24 @@ firebase.initializeApp({
 	serviceAccount: "../portfolio-882af-firebase-adminsdk-6c192-58ed2d06ad.json",
 });
 
-
+// обработка запроса работ из портфолио
 router.get('/works', (req, response) => {
-	firebase.database(firebase.app()).ref('works/').once('value').then((res) =>{
-		if(res.exists()) {
-			let data = res.val();
-			console.log("запрос списка работ");
-			response.json(JSON.stringify(data));
-		}
-		else console.log("error")
-	})
+	try {
+		firebase.database(firebase.app()).ref('works/').once('value').then((res) =>{
+			if(res.exists()) {
+				let data = res.val();
+				console.log("запрос списка работ");
+				response.json(JSON.stringify(data));
+			}
+			else {
+				console.log("error")
+				response.status(500).end()
+			}
+		})
+	}
+	catch {
+		response.status(500).end()
+	}
 })
 
 module.exports = router;
